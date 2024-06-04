747c93ea17fcf64ff0067dc02b738453488097fc
drsplashfire <gandlaroopesh48@gmail.com>
2024-06-05 00:42:14 +0530

    added routes

---
+++ CRUDtasks/Server/ValidateServer.cs
@@ -0,0 +1,14 @@
+﻿using CRUDtasks.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CRUDtasks.Server
+{
+    public class ValidateServer
+    {
+        public bool PersonExists(AppDbContext context, int id)
+        {
+            return context.Persons.Any(e => e.Id == id);
+        }
+    }
+}
